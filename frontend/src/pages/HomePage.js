d635335370b8f589bf62fbb9645dfd71e395f335
TheCakeBoutique <thecakeboutiques@hotmail.com>
2025-03-02 20:58:52 +0000

    Initial project setup

---
+++ frontend/src/pages/HomePage.js
@@ -0,0 +1,12 @@
+// frontend/src/pages/HomePage.js
+import React from 'react';
+
+const HomePage = () => {
+  return (
+    <div>
+      <h1>Welcome to AutoTrader Clone</h1>
+    </div>
+  );
+};
+
+export default HomePage;
